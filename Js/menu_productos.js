66f0be461a689e01e23869e88aacf1a6d6fc0b79
Diego Erazo <alux.0684@gmail.com>
2022-10-22 02:58:50 -0300

    funcionalidad de menu productos, coneccion archivo JSON y algunos estilos

--- Js/menu_productos.js
+++ Js/menu_productos.js
@@ -1,73 +1,69 @@
+//direccion del archivo JSON.
+const url = "/Js/db.json";
 
-let base_de_datos =[
-    {   
-        imagen:"/img/carrito/Bajonera.jpeg",
-        descripcion:"Bajonera",
-        precio:375.00,
-        id:1,
-        cantidad: 1,
-    },
-    {
-        imagen:"/img/carrito/Taparterias.jpeg",
-        descripcion:"Taparterias",
-        precio:500.00,
-        id:2,
-        cantidad: 1,
-    },
-    {
-        imagen:"/img/carrito/Palta Burger.jpeg",
-        descripcion:"Palta Burger",
-        precio:650.00,
-        id:3,
-        cantidad: 1,
-    },
-   
-   
- 
-];
-
-//array donde se gusrdan datos del carrito
-const carrito=[];
+//array donde se guardan productos a mostrar.
+let base_de_datos =[];
 
 
-//captura el div contenedor donde se insertan las carts
-const contenedor_de_productos=document.getElementById("contenedor_productos");
-/**
- * recorre la base de datos y crea los productos
- */
-base_de_datos.forEach((producto)=>{
-    const div=document.createElement('div');
-    div.classList.add('producto');
-    div.innerHTML=`
-    <div class="cart-div">
-        <img src="${producto.imagen}" alt=""class="cart-img">
-    </div>
-    <h3>${producto.descripcion}</h3>
-    <p>$${producto.precio}</p>
-    <button id="agregar${producto.id}" class="btn-agregar">Agregar Carrito</button>
-    `;
-    contenedor_de_productos.appendChild(div);
+//array donde se guardan datos del carrito
+let carrito=[];
 
 
-    //crea los eventos de los botones agregar
-    const btn_agregar=document.getElementById(`agregar${producto.id}`);
-    btn_agregar.addEventListener("click",()=>{
-        agregar_carrito(producto.id);
+// Funcion encargada de cargar los productos en la pagina
+function cargarProductos( productos ){
+
+    //captura el div contenedor donde se insertan las carts
+    const contenedor_de_productos=document.getElementById("contenedor_productos");
+    /**     
+    * recorre la base de datos y crea los productos
+    */
+    productos.forEach((producto)=>{
+        const div=document.createElement('div');
+        div.classList.add("producto");
+        div.classList.add("col-m-3");
+        div.innerHTML=`
+            <div class="img-contenedor">
+                <img src="${producto.imagen}" alt="" class="cart-img">
+            </div>
+            <div class="contenedor-texto">
+                <div>
+                    <h3>${producto.nombre}</h3>
+                </div>
+                <div>
+                    <p>${producto.descripcion}</p>
+                </div>
+                <div>
+                    <h3>$${producto.precio}</h3>
+                </div>
+                <div class="contenedor-btn">
+                    <button class="btn-agregar" id="agregar${producto.id}">Agregar al Carrito</button> 
+                </div>
+
+            </div>`;
+        contenedor_de_productos.appendChild(div);
+
+
+        //crea los eventos de los botones agregar
+        const btn_agregar=document.getElementById(`agregar${producto.id}`);
+        btn_agregar.addEventListener("click",()=>{
+         agregar_carrito(producto.id);
+        });
     });
-});
+
+}
+
 /**
- * captura el objeto para agregar al carrito
- * utiliza la propiedad id 
- */
+* captura el objeto para agregar al carrito
+* utiliza la propiedad id 
+*/
 const agregar_carrito=(producto_id)=>{
   
    
     const item= base_de_datos.find((prod) => prod.id === producto_id);
     carrito.push(item);
-    console.log(carrito);    
+        
    
     actualizar_carrito();
-
     Toastify({
 
         text: "Se agrego al carrito.",
@@ -76,19 +72,18 @@ const agregar_carrito=(producto_id)=>{
         
     }).showToast();
 
-
 };
 
 /**
- * capturando datos del html para funciones de la ventana del carrito
- */
+* capturando datos del html para funciones de la ventana del carrito
+*/
 let modal_carrito=document.getElementById("modal-carrito-contenedor");
 let btn_cerrar=document.getElementById("cerrar-carrito");
 let btn_modal_carrito=document.getElementById("btn-carrito");
 var body=document.getElementById("body");
 /**
- * evento del boton carrito
- */
+* evento del boton carrito
+*/
 btn_modal_carrito.addEventListener("click",()=>{
     modal_carrito.style.display="block";
     body.style.position="static";
@@ -98,8 +93,8 @@ btn_modal_carrito.addEventListener("click",()=>{
 
     
 /**
- * evento del boton cerrar carrito
- */
+* evento del boton cerrar carrito
+*/
 btn_cerrar.addEventListener("click",()=>{
     modal_carrito.style.display="none";
     body.style.position="inherit";
@@ -108,8 +103,8 @@ btn_cerrar.addEventListener("click",()=>{
 
 });
 /**
- * captura el contenedor para guardar datos del carrito
- */
+* captura el contenedor para guardar datos del carrito
+*/
 const contenedor_carrito=document.getElementById("carrito-contenedor");
 //captura el contador del carrito
 const contador_carrito=document.getElementById("contador-carrito");
@@ -122,10 +117,10 @@ function actualizar_carrito(){
     limpiar_campo();
     carrito.forEach((prod)=>{
         const div=document.createElement('div');
-        div.className="fila-carrito";
+        div.className="fila-carrito"
         div.innerHTML=`
         <img src="${prod.imagen}" alt="" width=40px height=40px>
-        <p>${prod.descripcion}</p>
+        <p>${prod.nombre}</p>
         <p> Cantidad: ${prod.cantidad}</p>
         <p>Precio: $${prod.precio}</p>
         <a href="javascript:eliminar_producto(${prod.id})">
@@ -144,6 +139,15 @@ function actualizar_carrito(){
  */
 function limpiar_campo(){
     const div=document.getElementById("carrito-contenedor");
+    div.innerHTML=` `
+        
+};
+
+/**
+ * limpia el contenedor para una nueva actualizacion
+*/
+function limpiar_contenedor(){
+    const div=document.getElementById("contenedor_productos");
     div.innerHTML=` `
         
 };
@@ -179,7 +183,6 @@ const btn_vaciar_carrito=document.getElementById("vaciar-carrito");
 btn_vaciar_carrito.addEventListener("click",()=>{
     carrito.length=0;
     actualizar_carrito();
-    
     Toastify({
 
         text: "El carrito esta vacio.",
@@ -188,3 +191,41 @@ btn_vaciar_carrito.addEventListener("click",()=>{
         
     }).showToast();
 });
+/**
+*carga los datos de el archivo json. 
+*/
+async function iniciar(){
+    fetch(url)
+        .then(respuesta =>respuesta.json())
+        .then(resultado =>{
+            //carga la lista de datos en el localStorage.
+            let lista_productos = JSON.stringify(resultado.productos)
+            localStorage.setItem("lista_productos",lista_productos)
+            
+            //guarda la lista de productos en una variable.
+            base_de_datos = JSON.parse(localStorage.getItem("lista_productos"));
+            //Muestra solo los productos destacados.
+            let productosDestacados = base_de_datos.filter( prod => prod.destacado == 1 );
+            cargarProductos(productosDestacados)
+        })
+        
+   
+};
+
+
+
+
+/*
+ * busca segun la categoria del parametro indicado
+ * @param {} dato 
+*/
+function filtrar(dato){
+    
+    let productos_para_picar=base_de_datos.filter( prod => prod.id_categoria == dato);
+    limpiar_contenedor();
+    cargarProductos(productos_para_picar);
+    
+
+};
+
+iniciar();
